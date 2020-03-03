b485bb4446f4557ce02ecdd3642b738972abf489
Steven Kristianto <stevenkristianto97@yahoo.com>
2020-03-03 15:17:19 +0700

    spring boot initializr

---
+++ src/main/java/com/bos/offlinetransaction/OfflinetransactionApplication.java
@@ -0,0 +1,13 @@
+package com.bos.offlinetransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflinetransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfflinetransactionApplication.class, args);
+	}
+
+}
